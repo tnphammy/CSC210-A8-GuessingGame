146c307debbfacc5ba2a136e7729e2b417ae3b0b
tnphammy <tnpham@smith.edu>
2025-11-15 16:46:52 -0500

    working interpret() function to process user responses more effectively

--- AnimalGuess.java
+++ AnimalGuess.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 /** Contains Animal Guessing Game*/
 public class AnimalGuess {
+    /**
+     * Inserts a new animal into the tree for more details
+     * @param tree the decision tree
+     * @param question the provided question
+     * @param answer the corresponding answer 
+     */
+    public void improveTree(DecisionTree tree, String question, String answer) {
+        
+    }
+    
+    /**
+     * Interprets a user's response into a boolean value
+     * @param answer the user's response string
+     * @return true or false
+     */
+    public static Boolean interpret(String answer) {
+        answer.toLowerCase();
+        if (answer.equals("yes") || answer.equals("y")) {
+            return true;
+        }
+        else if (answer.equals("no") || answer.equals("n")) {
+            return false;
+        }
+        else {
+            throw new UnsupportedOperationException("Unsupported input.");
+        }
+    }
     public static void main(String[] args) {
         // Set up binary tree
         DecisionTree tree = new DecisionTree("Is it a Mammal?");
@@ -14,46 +41,55 @@ public class AnimalGuess {
         tree.getLeft().getRight().getLeft().setRight(new DecisionTree("Cat"));
         // System.out.println(tree.followPath("Y").getData());
 
-
-        Scanner user = new Scanner(System.in); // set up Scanner object
-        DecisionTree curr = tree; // start at the root
-        while (curr.isBranch()) {
-            // Talk to user
-            System.out.println(curr.getData());
-            // Get user response
-            String response = user.nextLine().toLowerCase();
-            // Interpret response
-            if (response.equals("yes") || response.equals("y")) {
-                response = "Y";
-            }
-            else if (response.equals("no") || response.equals("n")) {
-                response = "N";
-            }
-            // Follow user's response on the DecisionTree
-            if(response.equals("Y") || response.equals("N")) {
-                curr = curr.followPath(response);
-            }
-            else {
-                throw new UnsupportedOperationException("Invalid response.");
+        Boolean gameContinues = true; /* ensures game loop */
+        try (Scanner user = new Scanner(System.in)) { // set up Scanner object
+            while (gameContinues) {
+                DecisionTree curr = tree; // start at the root
+                while (curr.isBranch()) {
+                    // Talk to user
+                    System.out.println(curr.getData());
+                    // Get user response
+                    String response = user.nextLine().toLowerCase();
+                    // Interpret response
+                    if (interpret(response)) {
+                        response = "Y";
+                    }
+                    else if (!interpret(response)) {
+                        response = "N";
+                    }
+                    // Follow user's response on the DecisionTree
+                    if(response.equals("Y") || response.equals("N")) {
+                        curr = curr.followPath(response);
+                    }
+                    else {
+                        throw new UnsupportedOperationException("Invalid response.");
+                    }
+                }
+                // Checks Program Answer
+                if (curr.isLeaf()) {
+                    System.out.println("Is it a " + curr.getData() + "?");
+                }
+                String grader = user.nextLine().toLowerCase();
+                if (interpret(grader)) {
+                    System.out.println("Cool! This was fun.");
+                }
+                else if (!interpret(grader)) {
+                    // Obtains new animal
+                    System.out.println("Sorry for that. What was your animal?");
+                    String realAnimal = user.nextLine();
+                    // Obtains question 
+                    System.out.println("Type a yes or no question that would distinguish between a " + curr.getData() + " and a " + realAnimal + ".");
+                    String betterQuestion = user.nextLine();
+                    // Obtains corresponding answer
+                    System.out.println("Would you answer yes to this question for the " + realAnimal + "?");
+                    String betterAnswer = user.nextLine().toLowerCase(); 
+                }
+                System.out.println("Play again?");
+                String userWish = user.nextLine().toLowerCase();
+                if (!interpret(userWish)) {
+                    gameContinues = false;
+                }
             }
         }
-        // Checks Program Answer
-        if (curr.isLeaf()) {
-            System.out.println("Is it a " + curr.getData() + "?");
-        }
-        String grader = user.nextLine();
-        if (grader.equals("yes") || grader.equals("y")) {
-            System.out.println("Cool! This was fun.");
-            user.close();
-        }
-        else if (grader.equals("no") || grader.equals("n")) {
-            System.out.println("Sorry for that. What was your animal?");
-            String realAnswer = user.nextLine();
-            System.out.println("Type a yes or no question that would distinguish between a " + curr.getData() + " and a " + grader + ".");
-            String betterQuestion = user.nextLine();
-            // TO DO: write code to integrate user question into tree.
-        }
-        user.close();
-
     }
 }
